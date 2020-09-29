709b3c1e4f6a1b63a0ac615c6958dc333944b8c4
Suvansh Kumar <suvansh.kumar07@gmail.com>
2020-09-29 14:29:39 -0500

    Correcting compilation errors, adding sample main
    Including the required headers, adding a sample main function to drive and test the code, the implementations of the called functions remains to be done

--- FileSystem.c
+++ FileSystem.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
 
 #define BLOCK_SIZE 1024
 #define MAX_FILE_SIZE 4194304 // 4GB of file size
@@ -64,17 +74,94 @@ void copyOut(char* destinationFilePath, char* fileName);
 void removeFile(char* fileName);    // Implements the rm command
 void removeDirectory(char* directoryName);
 void openFileSystem(const char *fileName);
-void initfs(char* filePath, int totalNumberOfBlocks, int totalNumberOfINodes)
+void initfs(char* filePath, int totalNumberOfBlocks, int totalNumberOfINodes);
 void quit();
 
 Inode getInode(int INumber) {
     Inode inode;
     lseek(fileDescriptor, 2 * BLOCK_SIZE + INumber * INODE_SIZE, SEEK_SET);
-    read(fileDescriptor, %inode, INODE_SIZE);
+    read(fileDescriptor, &inode, INODE_SIZE);
     return inode;
 }
 
 void quit() {
     close(fileDescriptor);
     exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    char c;
+
+    printf("\n Clearing screen \n");
+    system("clear");
+
+    unsigned int block_number =0, inode_number=0;
+    char *fs_path;
+    char *arg1, *arg2;
+    char *my_argv, cmd[256];
+
+    while(1)
+    {
+        printf("\n%s@%s>>>",fileSystemPath,currentWorkingDirectory);
+        scanf(" %[^\n]s", cmd);
+        my_argv = strtok(cmd," ");
+
+        if(strcmp(my_argv, "initfs")==0)
+        {
+
+            fs_path = strtok(NULL, " ");
+            arg1 = strtok(NULL, " ");
+            arg2 = strtok(NULL, " ");
+            if(access(fs_path, X_OK) != -1)
+            {
+                printf("filesystem already exists. \n");
+                printf("same file system will be used\n");
+            }
+            else
+            {
+                if (!arg1 || !arg2)
+                    printf(" insufficient arguments to proceed\n");
+                else
+                {
+                    block_number = atoi(arg1);
+                    inode_number = atoi(arg2);
+                    initfs(fs_path,block_number, inode_number);
+                }
+            }
+            my_argv = NULL;
+        }
+        else if(strcmp(my_argv, "q")==0){
+            quit();
+        }
+        else if(strcmp(my_argv, "ls")==0){
+            ls();
+        }
+        else if(strcmp(my_argv, "mkdir")==0){
+            arg1 = strtok(NULL, " ");
+            makeDirectory(arg1);
+        }
+        else if(strcmp(my_argv, "cd")==0){
+            arg1 = strtok(NULL, " ");
+            changedir(arg1);
+        }
+        else if(strcmp(my_argv, "cpin")==0){
+            arg1 = strtok(NULL, " ");
+            arg2 = strtok(NULL, " ");
+            copyIn(arg1,arg2);
+        }
+        else if(strcmp(my_argv, "cpout")==0){
+            arg1 = strtok(NULL, " ");
+            arg2 = strtok(NULL, " ");
+            copyOut(arg1,arg2);
+        }
+        else if(strcmp(my_argv, "rm")==0){
+            arg1 = strtok(NULL, " ");
+            rm(arg1);
+        }
+        else if(strcmp(my_argv, "currentWorkingDirectory")==0){
+            printf("%s\n",currentWorkingDirectory);
+        }
+
+    }
 }
